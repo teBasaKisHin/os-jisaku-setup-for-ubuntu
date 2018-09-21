1a07fb6b8ab1a02abbef4ffd45b8e01be55d43e2
tebasakioisii <tebasaki@tebasakiMBP.local>
2018-09-21 14:50:40 +0900

    write setup and readme

---
+++ README.md
@@ -0,0 +1,2 @@
+大神悠真著"フルスクラッチで作るOS自作Part1 http://yuma.ohgami.jp/x86_64-Jisaku-OS.pdf を参照して、作成
+Dockerなどで環境を簡単に移行できるように作成しました。
---
+++ setup.sh
@@ -0,0 +1,4 @@
+apt install build-essential qemu-system-x86 ovmf wget unzip vim
+git clone https://github.com/cupnes/x86_64_jisaku_os_samples
+wget http://yuma.ohgami.jp/poiboot_20180422.zip
+unzip poiboot_20180422.zip
